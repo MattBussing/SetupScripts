086f2485087f81d1c9bc10640119e0a13a7ba966
MattBussing <mbussing@mymail.mines.edu>
2018-06-26 14:32:06 -0600

    not sure what this is for

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sud
 #atom repository setup
 curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-
+sudo apt-get update
 #install packages
 sudo apt-get -y install git atom vim google-chrome-stable openvpn
 #install spotify
