4cd463ea3697fda6a16b09e16948a5bdb11ec058
MattBussing <mbussing@mymail.mines.edu>
2018-07-18 10:41:36 -0600

    added change to fix default viewer

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 sudo apt-get update
 sudo apt-get install okular
+grep -r "application/pdf=" "/usr/share/applications/defaults.list" "/usr/share/xubuntu/applications/defaults.list"
+sudo sed -ie 's/application\/pdf=.*/application\/pdf=okular.desktop/g' /usr/share/applications/defaults.list /usr/share/xubuntu/applications/defaults.list
+grep -r "application/pdf=" "/usr/share/applications/defaults.list" "/usr/share/xubuntu/applications/defaults.list"
