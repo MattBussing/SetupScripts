52609e4a9ec7c6e6d5eb40867ec588c7913e3364
MattBussing <mbussing@mymail.mines.edu>
2018-07-24 19:33:27 -0600

    fixed bug 1.0

--- installScripts/amino_setup/ompl.sh
+++ installScripts/amino_setup/ompl.sh
@@ -14,13 +14,16 @@ notes
 
 ./install-ompl-ubuntu.sh
 
+cd $HOME
 rm install-ompl-ubuntu.sh
+
 # create links for eigen for later use
 sudo ln -sf /usr/include/eigen3/Eigen /usr/local/include/Eigen
 sudo ln -sf /usr/include/eigen3/unsupported /usr/local/include/unsupported
 
+cd $SCRIPT
 echo "compiling test program to check for errors"
 g++ progam.cpp -o my_program
 ./my_program
 echo "did it compile and run?"
-cd $SCRIPT
+read answer
