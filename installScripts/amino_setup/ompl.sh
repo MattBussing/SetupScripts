f7a7c8933badaff35300556566abac1f4b7a15b1
MattBussing <mbussing@mymail.mines.edu>
2018-07-24 18:57:31 -0600

    added a test program

--- installScripts/amino_setup/ompl.sh
+++ installScripts/amino_setup/ompl.sh
@@ -13,3 +13,8 @@ rm install-ompl-ubuntu.sh
 # create links for eigen for later use
 sudo ln -sf /usr/include/eigen3/Eigen /usr/local/include/Eigen
 sudo ln -sf /usr/include/eigen3/unsupported /usr/local/include/unsupported
+
+echo "compiling test program to check for errors"
+g++ progam.cpp -o my_program
+./my_program
+echo "did it compile and run?"
